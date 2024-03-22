fa46598c74899c5acd4c6c2c45539abd936fe059
Evgeny Geyer <72138233+eutjeng@users.noreply.github.com>
2024-03-22 23:59:32 +0700

    change comment

--- src/features/userSelect/lib/utils/constants.ts
+++ src/features/userSelect/lib/utils/constants.ts
@@ -1,3 +1,3 @@
 export const ITEM_HEIGHT_IN_REM = 2; // Fixed height in REM for each item
 export const BUFFER_ITEMS = 5; // Number of items to render outside the viewport for smoother scrolling
-export const INITIAL_LIST_LIMIT = 50; // Initial number of items to fetch and render
+export const INITIAL_LIST_LIMIT = 50; // Initial number of items to fetch
