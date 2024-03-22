54303339e1a3d3437d946abe51fab26bb4d06215
Evgeny Geyer <72138233+eutjeng@users.noreply.github.com>
2024-03-23 00:41:48 +0700

    abstract inline styles to variables in UserList

--- src/features/userSelect/ui/UserList/index.tsx
+++ src/features/userSelect/ui/UserList/index.tsx
@@ -1,11 +1,14 @@
 import React, { useRef } from 'react';
 import { UserItem } from '../UserItem';
-
 import styles from './styles.module.css';
 import { User } from '@/shared/types/user';
 import { ITEM_HEIGHT_IN_REM } from '../../lib/utils/constants';
 import { usePartialListRendering } from '@/shared/hooks/usePartialListRendering';
 import { useInfiniteScroll } from '@/shared/hooks/useInfiniteScroll';
+
+const listStyle = {
+  paddingRight: '0.25rem',
+};
 
 interface UserListProps {
   users: User[];
@@ -28,6 +31,11 @@ export const UserList: React.FC<UserListProps> = ({
     users.length,
   );
 
+  const innerContainerStyle = {
+    height: `${users.length * ITEM_HEIGHT_IN_REM}rem`,
+    position: 'relative' as const,
+  };
+
   useInfiniteScroll({
     rootRef: listRef,
     onScrollToEnd: onLoadMore,
@@ -39,29 +47,25 @@ export const UserList: React.FC<UserListProps> = ({
       ref={listRef}
       className={styles.userList}
       onScroll={calculateVisibleItems}
-      style={{ paddingRight: '0.25rem' }}
+      style={listStyle}
     >
-      <div
-        style={{
-          height: `${users.length * ITEM_HEIGHT_IN_REM}rem`,
-          position: 'relative',
-        }}
-      >
+      <div style={innerContainerStyle}>
         {itemsToRender().map((index) => {
           const user = users[index];
+          const userItemStyle = {
+            position: 'absolute' as const,
+            top: `${index * ITEM_HEIGHT_IN_REM}rem`,
+            left: 0,
+            right: 0,
+            height: `${ITEM_HEIGHT_IN_REM}rem`,
+          };
 
           return (
             <UserItem
               key={user.id}
               user={user}
               isSelected={user.id === selectedUser?.id}
-              style={{
-                position: 'absolute',
-                top: `${index * ITEM_HEIGHT_IN_REM}rem`,
-                left: 0,
-                right: 0,
-                height: `${ITEM_HEIGHT_IN_REM}rem`,
-              }}
+              style={userItemStyle}
               onClick={() => onSelectUser(user)}
             />
           );
