cc83ab745c1d968e927d465ba8ed0ee6a6f330cf
Evgeny Geyer <72138233+eutjeng@users.noreply.github.com>
2024-03-23 01:31:50 +0700

    implement useFetch hook for data fetching

---
+++ src/shared/hooks/useFetch.ts
@@ -0,0 +1,42 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+export const useFetch = <T>(
+  fetchFunction: (page: number, limit: number) => Promise<T[]>,
+  initialPage: number = 0,
+  limit: number,
+) => {
+  const [data, setData] = useState<T[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [page, setPage] = useState<number>(initialPage);
+  const [hasMore, setHasMore] = useState<boolean>(true);
+  const isInitialLoad = useRef<boolean>(true);
+
+  const loadMore = useCallback(async () => {
+    setLoading(true);
+
+    const pageNumber = page + 1;
+
+    try {
+      const fetchedData = await fetchFunction(pageNumber, limit);
+
+      setData((prevData) => [...prevData, ...fetchedData]);
+      setHasMore(fetchedData.length === limit);
+      setPage(pageNumber);
+    } catch (e) {
+      setError('An error occurred while fetching data.');
+    } finally {
+      setLoading(false);
+    }
+  }, [fetchFunction, page, limit]);
+
+  useEffect(() => {
+    if (isInitialLoad.current && !loading) {
+      loadMore();
+
+      isInitialLoad.current = false;
+    }
+  }, [loadMore, loading]);
+
+  return { data, loading, error, hasMore, loadMore };
+};
